b64f1fbb144f8a86771e2d0092be08209652366a
Pedro Rigolon <pedrorigoloncaram@gmail.com.com>
2025-05-16 18:20:24 -0300

    UPDATE: Navbar scroll effect, responsivity global.

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -57,4 +57,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  window.addEventListener("scroll", function () {
+    let nav = document.querySelector(".navbar");
+
+    nav.classList.toggle('rolagem', window.scrollY > 0);
+    
+
+  });
 });
