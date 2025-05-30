b91caf748ea6b5e4e74b660de0bce7fbf5fef7bc
Pedro Rigolon <pedrorigoloncaram@gmail.com.com>
2025-05-30 02:24:31 -0300

    ADD: GET(/contents) retorna a lista de textos do site

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import axios from 'axios';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+import Content from "./models/Content.js";
 
 // Carregar variáveis de ambiente
 dotenv.config();
@@ -14,34 +15,34 @@ const apiEsports = axios.create({
   baseURL: process.env.APIESPORTS_URL,
   timeout: 5000, // Tempo máximo de espera em milissegundos
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': process.env.APIESPORTS_TOKEN
-  }
+    "Content-Type": "application/json",
+    Authorization: process.env.APIESPORTS_TOKEN,
+  },
 });
 
 // Middleware
 const corsOptions = {
-  origin: '*', // Permite todas as origens
-  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
-  credentials: true // Permite envio de cookies e credenciais
-}
+  origin: "*", // Permite todas as origens
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Métodos HTTP permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
+  credentials: true, // Permite envio de cookies e credenciais
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-
 // função para conectar o MongoDB
 async function conectarMongoDB() {
   try {
+    // Certifique-se que a URL já inclui o nome do banco: .../mauaesports-db
     const MONGODB_URL = process.env.MONGODB_URL;
 
     await mongoose.connect(MONGODB_URL);
     
-    console.log('Conectado ao MongoDB com sucesso!');
+    console.log("Conectado ao MongoDB com sucesso!");
   } catch (error) {
-    console.error('Erro na conexão com o MongoDB:', error.message);
-    process.exit(1); // Encerra o servidor em caso de erro na conexão
+    console.error("Erro na conexão com o MongoDB:", error.message);
+    process.exit(1);
   }
 }
 
@@ -50,37 +51,29 @@ async function conectarMongoDB() {
 
 // ROTAS
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API Mauá Esports funcionando!' });
-});
-
-
-// funcao que envia uma requisicao a api2 (apenas para testes)
-async function testeApi() {
+app.get("/content", async (req, res) => {
   try {
-    const res = await apiEsports.get('/modality/all');
+    // Usa o modelo 'Content' para encontrar todos os documentos na coleção 'contents'
+    const allContent = await Content.find({});
     
-    if(res.status !== 401) {
-      console.log(`Teste de envio de req Funcionou! Status Code: ${res.status}`);
-      
-    } 
-    
+    if (allContent.length === 0) {
+      return res.status(404).json({ message: 'Nenhum conteúdo encontrado na coleção.' });
+    }
+
+    res.status(200).json(allContent);
   } catch (error) {
-    console.error('Erro na requisição:', error.message);
+    res.status(500).json({ message: 'Erro no servidor', error: error.message });
   }
-}
-
-testeApi();
-
+});
 
 // Iniciando o servidor
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  try{
+  try {
     conectarMongoDB();
-    console.log(`Servidor funcionando na porta: ${PORT}`)
-  }catch(error){
-    console.error(`Erro ao conectarMongoDB: ${error}`)
+    console.log(`Servidor funcionando na porta: ${PORT}`);
+  } catch (error) {
+    console.error(`Erro ao conectarMongoDB: ${error}`);
   }
-}); 
+});
